3a3801cd31ad2e078848993953957481d0780ecf
hyprland <takumifahri@apps.ipb.ac.id>
2026-04-29 14:50:15 +0700

    fix

--- src/app/(main)/barter/page.jsx
+++ src/app/(main)/barter/page.jsx
@@ -3,7 +3,7 @@ export default function barter() {
     return (
         <div>
             <h1>Barter Page</h1>
-            <p>Welcome to the barter! Here you can exchange goods and services without using money.</p>
+            <p>Welcome to the barter! Here you can exchange goods and services without using money test.</p>
         </div>
     );
 }
